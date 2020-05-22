53ba572b322372b4a46d0ce1b6acd6437ca74eed
blank <1161709455@qq.com>
2020-05-22 13:46:53 +0800

    安卓 启动页面

--- android/app/src/main/java/com/demo/MainActivity.java
+++ android/app/src/main/java/com/demo/MainActivity.java
@@ -1,8 +1,15 @@
 package com.demo;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactA        ctivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
